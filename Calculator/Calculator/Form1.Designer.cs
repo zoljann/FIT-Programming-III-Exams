fac1bc783ba9c79f223f8bd64eef39cd3532c8f2
Nedim Zolj <74002374+zoljann@users.noreply.github.com>
2021-11-02 12:54:49 +0100

    AddNumbers

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -29,13 +29,261 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOne = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.btnJednako = new System.Windows.Forms.Button();
+            this.btnSabiranje = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnOduzimanje = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnMnozenje = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnDjeljenje = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnCE = new System.Windows.Forms.Button();
+            this.btnPostotak = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(0, 0);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(75, 23);
+            this.btnOne.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(0, 410);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(80, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnZero
+            // 
+            this.btnZero.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnZero.Location = new System.Drawing.Point(77, 410);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(80, 50);
+            this.btnZero.TabIndex = 1;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPoint.Location = new System.Drawing.Point(154, 410);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(80, 50);
+            this.btnPoint.TabIndex = 2;
+            this.btnPoint.Text = ",";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            // 
+            // btnJednako
+            // 
+            this.btnJednako.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnJednako.Location = new System.Drawing.Point(231, 410);
+            this.btnJednako.Name = "btnJednako";
+            this.btnJednako.Size = new System.Drawing.Size(80, 50);
+            this.btnJednako.TabIndex = 3;
+            this.btnJednako.Text = "=";
+            this.btnJednako.UseVisualStyleBackColor = true;
+            // 
+            // btnSabiranje
+            // 
+            this.btnSabiranje.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSabiranje.Location = new System.Drawing.Point(231, 363);
+            this.btnSabiranje.Name = "btnSabiranje";
+            this.btnSabiranje.Size = new System.Drawing.Size(80, 50);
+            this.btnSabiranje.TabIndex = 7;
+            this.btnSabiranje.Text = "+";
+            this.btnSabiranje.UseVisualStyleBackColor = true;
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnThree.Location = new System.Drawing.Point(154, 363);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(80, 50);
+            this.btnThree.TabIndex = 6;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTwo.Location = new System.Drawing.Point(77, 363);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(80, 50);
+            this.btnTwo.TabIndex = 5;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(0, 363);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 50);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "1";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // btnOduzimanje
+            // 
+            this.btnOduzimanje.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnOduzimanje.Location = new System.Drawing.Point(231, 317);
+            this.btnOduzimanje.Name = "btnOduzimanje";
+            this.btnOduzimanje.Size = new System.Drawing.Size(80, 50);
+            this.btnOduzimanje.TabIndex = 11;
+            this.btnOduzimanje.Text = "-";
+            this.btnOduzimanje.UseVisualStyleBackColor = true;
+            // 
+            // btnSix
+            // 
+            this.btnSix.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSix.Location = new System.Drawing.Point(154, 317);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(80, 50);
+            this.btnSix.TabIndex = 10;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            // 
+            // btnFive
+            // 
+            this.btnFive.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFive.Location = new System.Drawing.Point(77, 317);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(80, 50);
+            this.btnFive.TabIndex = 9;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFour.Location = new System.Drawing.Point(0, 317);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(80, 50);
+            this.btnFour.TabIndex = 8;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            // 
+            // btnMnozenje
+            // 
+            this.btnMnozenje.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMnozenje.Location = new System.Drawing.Point(231, 271);
+            this.btnMnozenje.Name = "btnMnozenje";
+            this.btnMnozenje.Size = new System.Drawing.Size(80, 50);
+            this.btnMnozenje.TabIndex = 15;
+            this.btnMnozenje.Text = "X";
+            this.btnMnozenje.UseVisualStyleBackColor = true;
+            // 
+            // btnNine
+            // 
+            this.btnNine.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNine.Location = new System.Drawing.Point(154, 271);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(80, 50);
+            this.btnNine.TabIndex = 14;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            // 
+            // btnEight
+            // 
+            this.btnEight.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEight.Location = new System.Drawing.Point(77, 271);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(80, 50);
+            this.btnEight.TabIndex = 13;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSeven.Location = new System.Drawing.Point(0, 271);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(80, 50);
+            this.btnSeven.TabIndex = 12;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            // 
+            // btnDjeljenje
+            // 
+            this.btnDjeljenje.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDjeljenje.Location = new System.Drawing.Point(231, 224);
+            this.btnDjeljenje.Name = "btnDjeljenje";
+            this.btnDjeljenje.Size = new System.Drawing.Size(80, 50);
+            this.btnDjeljenje.TabIndex = 19;
+            this.btnDjeljenje.Text = "/";
+            this.btnDjeljenje.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            this.btnC.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnC.Location = new System.Drawing.Point(154, 224);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(80, 50);
+            this.btnC.TabIndex = 18;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnCE
+            // 
+            this.btnCE.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCE.Location = new System.Drawing.Point(77, 224);
+            this.btnCE.Name = "btnCE";
+            this.btnCE.Size = new System.Drawing.Size(80, 50);
+            this.btnCE.TabIndex = 17;
+            this.btnCE.Text = "CE";
+            this.btnCE.UseVisualStyleBackColor = true;
+            // 
+            // btnPostotak
+            // 
+            this.btnPostotak.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPostotak.Location = new System.Drawing.Point(0, 224);
+            this.btnPostotak.Name = "btnPostotak";
+            this.btnPostotak.Size = new System.Drawing.Size(80, 50);
+            this.btnPostotak.TabIndex = 16;
+            this.btnPostotak.Text = "%";
+            this.btnPostotak.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 574);
+            this.ClientSize = new System.Drawing.Size(310, 460);
+            this.Controls.Add(this.btnDjeljenje);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnCE);
+            this.Controls.Add(this.btnPostotak);
+            this.Controls.Add(this.btnMnozenje);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnOduzimanje);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnSabiranje);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.btnJednako);
+            this.Controls.Add(this.btnPoint);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Calculator";
             this.ResumeLayout(false);
@@ -43,6 +291,28 @@ namespace Calculator
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Button btnJednako;
+        private System.Windows.Forms.Button btnSabiranje;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnOduzimanje;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnMnozenje;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnDjeljenje;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnCE;
+        private System.Windows.Forms.Button btnPostotak;
     }
 }
 
